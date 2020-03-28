67a1d0e5fb3e9354fa9148b94fb1406cce2b2324
Chaker Gamra <developer3.5pro@gmail.com>
2020-03-28 11:22:20 +0100

    Unity Endless Game - Part 15: Creating a Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
